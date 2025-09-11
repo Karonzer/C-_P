1495d781777355b68da0881a00e1e701c06b2bd5
Karonzer <knkhsu122@naver.com>
2025-09-11 19:42:59 +0900

    day 2 작업 업로드

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+using namespace std;
+
+//int packet = 200; // 전역 변수
+
+
+void Swap(int& _x, int& _y)
+{
+	int temp = _x;
+	_x = _y;
+	_y = temp;
+}
+
+
+void Swap2(int* _x, int* _y)
+{
+	int temp = *_x;
+	*_x = *_y;
+	*_y = temp;
+}
+
+
+int main()
+{
+#pragma region 스트림
+	//  시간의 흐름에 따라 연속족으로 발생하는 데이터의 흐름
+
+	//int num;
+	//cin >> num;
+
+	//for (int i = 1; i <= num; i++)
+	//{
+	//	cout << "Play Station " << i << endl;
+	//}
+	////cin >> num;
+
+	//cout << "Play Station " << num << endl;
+
+	//printf("sreies : %d\n", num);
+
+	// 스트림은 운영 체제에 의해 생성되며, 스트림 자체는 버퍼라는 
+	// 임시 메모리 공간이 존재합니다
+
+
+
+#pragma endregion
+
+#pragma region 범위 지정 연산자
+	// 여러 범위에서 사용되는 식별자를 구분하는데 
+	// 사용하는 연산자 입니다.
+
+	//int packet = 100; // 지역 변수
+
+	//cout << "packet 지역 변수의 값  : " << packet << endl; // 지역 변수
+
+	//cout << "packet 전역 변수의 값  : " << ::packet << endl; // 지역 변수
+
+	// 범위 지정 연산자는 전역 변수와 같은 이름의 지역 변수/
+	// 선언 되었을 때 가장 가까운 범위에 선언되는 번수의 이름은
+	// 사용하는 범위 규칙이  존재하기 때문에 전역 변수가 호출되지 않습니다.
+
+
+#pragma endregion
+
+#pragma region 참조자
+	// 하나의 변수에 또 다른 이름을 지정하는 지정자입니다.
+	//int x = 10;
+	//int y = 20;
+
+	//cout << "x : " << x << endl;
+	//cout << "y : " << y << endl;
+
+	//Swap(x, y);
+	//cout << "Swap(x, y)" << endl;
+	//cout << "x : " << x << endl;
+	//cout << "y : " << y << endl;
+
+	//Swap2(&x, &y);
+	//cout << "Swap2(&x, y)" << endl;
+	//cout << "x : " << x << endl;
+	//cout << "y : " << y << endl;
+#pragma endregion
+
+#pragma region 동적할당
+
+	//int num = 1;
+	//int* ptr = new int;
+	//*ptr = 10;
+
+	//cout << "*ptr : " << *ptr << endl;
+
+	//delete ptr;
+
+	//int* pointer = new int[5];
+	//for (int i = 0; i < 5;i++)
+	//{
+	//	pointer[i] = (i + 1) * 10;
+	//}
+
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	cout << "pointer[" << i << "] : " << pointer[i] << endl;
+	//}
+
+	//delete[] pointer;
+
+#pragma endregion
+
+#pragma region 
+#pragma endregion
+
+}
+
---
+++ UTC[class]/UTC[class].cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+using namespace std;
+
+namespace KR
+{
+	float area = 3.14f;
+
+	void Size()
+	{
+		cout << "KR area : " << area << "km" << endl;
+	}
+}
+
+using namespace KR;
+
+namespace JP
+{
+	float area = 551.1f;
+
+	void Size()
+	{
+		cout << "JP area : " << area << "km" << endl;
+	}
+}
+
+class Obj
+{
+#pragma region 접근 지정자
+	// 클래스의 멤버에 대한 접근 권한을 제한 하는 지정자입니다.
+	// public : 클래스 배누와 자기가 상속하고 있는 클래스 그리고 클래스 외부에서도 접근을 허용하는 지정자입니다.
+
+	// protected : 클래스 내부와 자기가 상속하고 있는 클래스에서만 접근을 허용하는 지정자입니다.
+
+	// private : 클래스 내부에서만 접근을 허용하는 지정자입니다. (기본값)
+private:
+	int x = 1;
+	int y = 2;
+	int z = 3;
+protected:
+	const char* name;
+public:
+
+	void Position()
+	{
+		cout << "x : " << x << endl;
+		cout << "y : " << y << endl;
+		cout << "z : " << z << endl;
+	}
+#pragma endregion
+};
+
+
+void Profile(const char* name = "test", int age = 20)
+{
+	cout << "Name : " << name << endl;
+	cout << "Age : " << age << endl;
+}
+
+int main()
+{
+#pragma region 이름 공간
+	//속성을 구분할 수 있도록 유효 범위를
+	//설정하는 영역입니다.
+
+	//Size();
+
+	//JP::Size();
+
+#pragma endregion
+
+#pragma region 클래스
+	//사용자 정의 데이터 유형으로 속성과 함수가 포함되어
+	//있으면 클래스를 통해 객체를 생성하여 접근하고 사용
+	//하는 집합체입나다.
+	// 
+	//Obj obj;
+	//obj.Position();
+
+	//클래스의 경우 클래스 내부에 있는 변수는 클래스의
+	//메모리 영역에 포함되지만. 정적 변수와 함수의 메모리는
+	//클래스 영역에 포함되지 않습니다.
+
+
+	//cout << "Obj의 크기 : " << sizeof(Obj) << endl;
+
+#pragma endregion
+
+#pragma region 기본 매개 변수
+	//함수의 매개 변수에 값이 전달되지 않을 때/
+	// 미리 값을 설정해놓는 매개 변수입니다.
+
+	//기본 매개 변수를 지정하려면 오른쪽에서 부터
+	// 값을 지정해주여야 합니다.
+
+	//Profile();
+
+	//Profile("KEG",18);
+#pragma endregion
+	return 0;
+}
+
