18a9992fa9a53febc5dd749833637c92f8fe9294
Karonzer <knkhsu122@naver.com>
2025-09-29 19:45:27 +0900

    c++ STL 백터 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,7 +1,58 @@
 ﻿#include "stdafx.h"
-
+#include <vector>
+#include <list>
 int main()
 {
+#pragma region 선형 컨테이너
+	// 데이터를 선형으로 저장하며 특별한 제약이나 규칭이 없는 컨테이입니다,
+#pragma region vector
+	
+
+	/*vector<int> vector;
+	vector.reserve(20);
+
+	vector.push_back(10);
+
+
+
+	vector.push_back(20);
+	vector.push_back(30);
+
+
+	vector.push_back(40);
+	vector.push_back(50);
+
+
+	cout << vector.capacity() << endl;
+
+	cout << "vector size : " << vector.size() << endl;
+	for (int i = 0; i < vector.size(); i++)
+	{
+		cout << vector[i] << endl;
+	}
+
+
+
+	vector.pop_back();
+
+	for (int i = 0; i < vector.size(); i++)
+	{
+		cout << vector[i] << endl;
+	}*/
+
+#pragma endregion
+
+#pragma region 리스트 컨테이너
+	list<int> list;
+
+	list.push_back(10);
+	list.push_back(20);
+	list.push_back(30);
+	list.push_back(40);
+	list.push_back(50);
+
+#pragma endregion
+#pragma endregion
 
 	return 0;
 }
