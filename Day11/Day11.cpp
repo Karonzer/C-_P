4da7b090a25f3d0b52353baacf8f8a63c403647a
Karonzer <knkhsu122@naver.com>
2025-10-01 19:44:57 +0900

    day 연관 컨테이너 업로드

---
+++ Day11/Day11.cpp
@@ -0,0 +1,80 @@
+﻿#include "../Program/stdafx.h"
+#include <stack>
+#include <queue>
+int main()
+{
+#pragma region 컨테이너 어댑터
+	// 기존 컨테이너의 인테페이스를 제한하여 만든 기능이
+	// 제한 되거나 변경된 컨테이너입니다.
+
+#pragma region 스택 컨테이너
+	//stack<int> stack;
+
+	//stack.push(10);
+	//stack.push(20);
+	//stack.push(30);
+	//stack.push(40);
+	//stack.push(50);
+
+	//cout << "size : " << stack.size() << endl;
+	////for (int i = 0; i < size;i++)
+	////{
+	////	cout << stack.top() << endl;
+	////	stack.pop();
+	////}
+
+	//while (!stack.empty())
+	//{
+	//	cout << stack.top() << endl;
+	//	stack.pop();
+	//}
+
+
+	//cout << "size : " << stack.size() << endl;
+#pragma endregion
+
+#pragma region 큐 컨테이너
+	//queue<int> queue;
+
+	//queue.push(10);
+	//queue.push(20);
+	//queue.push(30);
+	//queue.push(40);
+	//queue.push(50);
+
+	//cout << "size : " << queue.size() << endl;
+
+	//int size = queue.size();
+	//for (int i = 0; i < size;i++)
+	//{
+	//	cout << queue.front() << endl;
+	//	queue.pop();
+	//}
+
+#pragma endregion
+#pragma region 프라이어리 큐 컨테이너
+
+	//priority_queue<int> qu;
+	//qu.push(12);
+	//qu.push(1);
+	//qu.push(82);
+	//qu.push(56);
+	//qu.push(5);
+	//qu.push(77);
+	//qu.push(23);
+	//qu.push(47);
+	//qu.push(22);
+	//qu.push(8);
+
+	//while (!qu.empty())
+	//{
+	//	cout << qu.top() << endl;
+	//	qu.pop();
+	//}
+
+
+#pragma endregion
+
+#pragma endregion
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,56 +1,34 @@
 ﻿#include "stdafx.h"
-#include <stack>
-#include <queue>
+#include <unordered_set>
+
 int main()
 {
-#pragma region 컨테이너 어댑터
-	// 기존 컨테이너의 인테페이스를 제한하여 만든 기능이
-	// 제한 되거나 변경된 컨테이너입니다.
+#pragma region 연관 컨테이너
+	//특정한 방식으로 데이터를 저장하고 검색하는 컨테이너입니다.
 
-#pragma region 스택 컨테이너
-	//stack<int> stack;
+#pragma region unordered_set
 
-	//stack.push(10);
-	//stack.push(20);
-	//stack.push(30);
-	//stack.push(40);
-	//stack.push(50);
+	unordered_set<const char*> set;
 
-	//cout << "size : " << stack.size() << endl;
-	////int size = stack.size();
-	////for (int i = 0; i < size;i++)
-	////{
-	////	cout << stack.top() << endl;
-	////	stack.pop();
-	////}
+	set.reserve(16);
+	set.max_load_factor(2.0);
 
-	//while (!stack.empty())
-	//{
-	//	cout << stack.top() << endl;
-	//	stack.pop();
-	//}
+	set.insert("검");
+	set.insert("방패");
+	set.insert("신발");
+	set.insert("지팡이");
+	set.insert("갑옷");
+	set.insert("모자");
+	set.insert("망토");
+	set.insert("구슬");
+	set.insert("포션");
 
-	
-	//cout << "size : " << stack.size() << endl;
-#pragma endregion
+	cout << "set.set.load_factor() : " << set.load_factor() << endl;
+	cout << "set.bucket_count() : " << set.bucket_count() << endl;
 
-#pragma region 큐 컨테이너
-	queue<int> queue;
 
-	queue.push(10);
-	queue.push(20);
-	queue.push(30);
-	queue.push(40);
-	queue.push(50);
 
-	cout << "size : " << queue.size() << endl;
 
-	int size = queue.size();
-	for (int i = 0; i < size;i++)
-	{
-		cout << queue.front() << endl;
-		queue.pop();
-	}
 
 #pragma endregion
 #pragma endregion
